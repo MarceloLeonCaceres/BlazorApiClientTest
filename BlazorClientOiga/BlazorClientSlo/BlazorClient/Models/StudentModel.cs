428e0719c14803428185f99729bd7b4174ea7739
MarceloLeonCaceres <marcelo.leon.c@gmail.com>
2023-04-23 20:27:54 -0500

    Version con Student Courses and Evaluations

---
+++ BlazorClientOiga/BlazorClientSlo/BlazorClient/Models/StudentModel.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorClient.Models;
+
+public class StudentModel
+{
+    public Guid Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? LastName { get; set; }
+
+    public DateTime? CreationDate { get; set; }
+
+    public bool? Active { get; set; }
+}
